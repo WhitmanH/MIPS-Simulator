8c5f7a8ad0cac8007d48c08c333032fc5d944cf6
Haley Whitman <hwhitman@uoregon.edu>
2015-11-07 02:01:46 -0800

    fixing git

---
+++ ALU/ALU.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../constants.h"
+#include "./ArithmticUnits.h"
+#include "ALUhelpers.c"
+
+
+int32_t ALU(uint8_t opcode, int32_t operand_A, int32_t operand_B, int *status_out) {
+    int32_t out;
+
+    if (opcode == ALU_NOP)
+        return 0;
+
+    switch(opcode){
+        case ALU_ADD:
+            if (add_overflow(operand_A, operand_B)) {
+                set_signal_integer_overflow(*status_out);
+            }
+            out = operand_A + operand_B;
+            break;
+        case ALU_ADDU:
+            out = operand_A + operand_B;
+            break;
+        case ALU_SUB:
+            out = operand_A - operand_B;
+            break;
+        case ALU_SUBU:
+            if (sub_overflow(operand_A, operand_B)){
+                set_signal_integer_overflow(*status_out);
+            }
+            out = operand_A - operand_B;
+            break;
+        case ALU_AND:
+            out = operand_A & operand_B;
+            break;
+        case ALU_OR:
+            out = operand_A | operand_B;
+            break;
+        case ALU_XOR:
+            out = (operand_A ^ operand_B);
+            break;
+        case ALU_LZ:
+            out = (operand_A < 0);
+            break;
+        case ALU_GZ:
+            out = (operand_A > 0);
+            break;
+        case ALU_LEZ:
+            out = (operand_A <= 0);
+            break;
+        case ALU_GEZ:
+            out = (operand_A >= 0);
+            break;
+        case ALU_DIF:
+            out = (operand_A != operand_B);
+            break;
+        case ALU_EQ:
+            out = (operand_A == operand_B);
+            break;
+        case ALU_SLT:
+            out = (operand_A < operand_B);
+            break;
+        case ALU_SLTU:
+            out = ((uint32_t) operand_A <  (uint32_t) operand_B);
+            break;
+        case ALU_SHIFT16:
+            out = operand_B << 16;
+            break;
+        case ALU_NOR:
+            out = ~(operand_A | operand_B);
+            break;
+        case ALU_MUL:
+            out = operand_A * operand_B;
+            break;
+        default:
+            fprintf(stderr, "case da alu\n");
+            exit(1);
+    }
+
+    if (out == 0)
+        set_signal_zero_out(*flags);
+
+    return out;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,33 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #define MAX_LINE 100
 #define MAX_LENGTH 100
 
 int main()
 {
     printf("Hello world here's a test commit!\n");
-    FILE *pointerFile;
-    char buffer[MAX_LINE]; // max length of one line of code
-    char* instructionMIPS[MAX_LENGTH]; //100 lines of code
+    char *fileToRead = "function.asm";
+    parseMIPS(fileToRead);
 
-    pointerFile = fopen("function.asm", "r"); //using the functions MIPS code to test.
-    if(!pointerFile){
-        printf("File not found");
-        return 0;
-    }
 
-    int i = 0;
-    while (fgets(buffer, MAX_LINE, pointerFile) != NULL){
-        printf("%s", buffer);
-        instructionMIPS[i] = strdup(buffer); //string duplicate
-        i++;
-    }
-    fclose(pointerFile);
-
-    //checking to see if saved to instructionMIPS
-    printf("check to see if a line is saved to array: %s", instructionMIPS[12]);
-    printf("\ndone");
+    printf("\nFinished Main");
 
     return 0;
 }
+
+void parseMIPS(char *file){
+    char* MIPSInstructions[MAX_LENGTH][MAX_LENGTH];
+    FILE *pointerFile;
+
+<<<<<<< HEAD
+    char *token;
+    char *delimiter = " ,\t\r\n";
+
+    pointerFile = fopen(file, "r");
+    if(pointerFile == NULL){
+        printf("file not found");
+=======
+    pointerFile = fopen("/Users/rui/Google Drive/UO/Fall 2015/CIS 314/Processor-Assignment1/MispTestFiles/function.asm", "r"); //using the functions MIPS code to test.
+    if(!pointerFile){
+        printf("File not found");
+        return 0;
+>>>>>>> origin/master
+    }
+
+    fseek(pointerFile, 0, SEEK_SET);
+    int i = 0; //start from second part in the array, as 0 is only for the label.
+    while(i < MAX_LINE && fgets(MIPSInstructions[i], MAX_LINE, pointerFile)){
+        for(token = strtok(MIPSInstructions[i], delimiter); token; token=strtok(NULL, delimiter)){
+            if(token[0] == '#'){ // a comment
+                break;
+            }
+
+            if(token[strlen(token)-1] == ':'){ //labels end with :
+                //MIPSInstructions[0] = token;
+                //break;
+            }
+            MIPSInstructions[i][strlen(MIPSInstructions[i])-1] = '\0';//get rid of end character.
+            i++;
+            printf("[%s]", token);
+        }
+        printf("\n");
+    }
+    fclose(pointerFile);
+
+    //printf("example test = [%s]", MIPSInstructions[13][3]);
+
+}
+
+//PREVIOUS IMPLEMENTATION USED THIS. Still keeping just in case.
+/*
+int isBlankLine(const char *line) {
+    const char accept[]=" \t\r\n"; // any white spaces or simnly \n.
+    return (strspn(line, accept) == strlen(line));
+}
+
+void trimWhiteSpace(char *line) {
+    char *newLine = line;
+    int l = strlen(newLine);
+
+    while(isspace(newLine[l - 1])) newLine[--l] = 0;
+    while(*newLine && isspace(*newLine)) ++newLine, --l;
+
+    memmove(line, newLine, l + 1);
+}
+*/
